99c7f57d54dcf9838639bd62c0a7e4919c64daa5
bfmq <617351843@qq.com>
2019-11-12 14:17:31 +0800

    day6 dir make

---
+++ day6/class_code.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# __author__ = '北方姆Q'
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from sklearn.datasets import load_boston
+from sklearn.utils import resample
+from keras.layers import Dense
+from keras.models import Sequential
