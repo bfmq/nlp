d99fbcf9019a3636397cf2d430d72eb32d5088d6
bfmq <617351843@qq.com>
2019-11-23 23:20:48 +0800

    改成自己的理解备注

--- day6/homework2.py
+++ day6/homework2.py
@@ -6,25 +6,20 @@ import numpy as np
 
 
 def conv_single_step(a_prev_slice, W, b):
-    '''
-    Apply one filter defined by parameters W on a single slice (a_slice_prev) of the output activation
-    of the previous layer.
-    Arguments:
-    a_prev_slice: slice of input data (shape=(f,f,n_C_prev))
-    W: Weight parameters contained in a window. (shape = (f,f,n_C_prev))
-    b: Bias parameters contained in a window. (shape=(1,1,1))
-
-    Reutrns:
-
-    Z: a scalar value, the result of convolving the sliding window (W, b) on a slice x of the input data
-    '''
-    # Element-wise product
+    """
+    计算单步卷积操作
+    :param a_prev_slice: 原单步的一个小矩阵
+    :param W: 卷积核
+    :param b: 常量
+    :return: 经过卷积核计算后结果
+    """
+    # 与卷积核点乘后的小矩阵
     s = a_prev_slice * W
 
-    # Sum over s
+    # 小矩阵数值总和
     Z = np.sum(s)
 
-    # Add bias b to z.
+    # 加伤常量
     Z += b
 
     return Z
@@ -40,20 +35,14 @@ def conv_single_step(a_prev_slice, W, b):
 
 def zero_pad(X, pad):
     """
-    Pad with zeros all images of the dataset X. The padding is applied to the height and width of an image,
-    as illustrated in Figure 1.
-
-    Argument:
-    X: python numpy array of shape (m, n_H, n_W, n_C) representing a batch of m images
-    pad: integer, amount of padding around each image on vertical and horizontal dimensions
-
-    Returns:
-    X_pad: padded image of shape (m, n_H + 2*pad, n_W + 2*pad, n_C)
+    为角落里的像素提高使用次数，增加边缘信息，填充0可以保持卷积计算前后卷的高和宽不变化
+    :param X: 原维度
+    :param pad: 填充值
+    :return: 填充后的维度
     """
-
     X_pad = np.pad(X, ((0, 0), (pad, pad), (pad, pad), (0, 0)), 'constant')
-
     return X_pad
+
 
 # np.random.seed(1)
 # x = np.random.randn(4, 3, 3, 2)
@@ -66,68 +55,55 @@ def zero_pad(X, pad):
 
 def conv_forward(A_prev, W, b, hparameters):
     """
-    Implements the forward propagation for a convolution function
-
-    Arguments:
-    A_prev: output activations of the previous layer,
-        numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
-    W: Weights, numpy array of shape (f, f, n_C_prev, n_C)
-    b: Biases, numpy array of shape (1, 1, 1, n_C)
-    hparameters: python dictionary containing "stride" and "pad"
-
-    Returns:
-    Z: conv output, numpy array of shape (m, n_H, n_W, n_C)
-    cache: cache of values needed for the conv_backward() function
+    将整个矩阵分别进行conv_single_step，这样最终获得的就是卷积后的整个矩阵
+    :param A_prev: 完全的原矩阵
+    :param W: 卷积核
+    :param b: 常量
+    :param hparameters: 其他参数，如步长、填充空白
+    :return: 经过卷积核后的矩阵
     """
-
-    # Get dimensions from A_prev's shape
+    # 原矩阵维度
     (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
 
-    # Get dimensions from W's shape
+    # 卷积核维度
     (f, f, n_C_prev, n_C) = W.shape
 
-    # Get information from "hparameters"
+    # 获取hparameters里的步长与填充值
     stride = hparameters.get('stride', 1)
     pad = hparameters.get('pad', 0)
 
-    # Compute the dimensions of the CONV output volume using the formula given above.
-    # Hint: use int() to apply the 'floor' operation.
+    # 步长与填充值会影响我们的单步卷积的取值，提前计算好
     n_H = int((n_H_prev - f + 2*pad) / stride + 1)
     n_W = int((n_W_prev - f + 2*pad) / stride + 1)
 
-    # Initialize the output volume Z with zeros.
+    # 预设应返回的经过卷积核后的矩阵大小
     Z = np.zeros((m, n_H, n_W, n_C))
 
-    # Create A_prev_pad by padding A_prev
+    # 将Z进行填充操作
     A_prev_pad = zero_pad(A_prev, pad)
 
-    # 4步循环等于把整个元数据取了一遍
-    for i in range(m):  # loop over the batch of training examples
+    # 4步循环等于把整个元数据的每个维度取了一遍
+    for i in range(m):  # 循环所有
         a_prev_pad = A_prev_pad[i, :, :, :]  # Select ith training example's padded activation
-        for h in range(n_H):  # loop over vertical axis of the output volume
-            # Find the vertical start and end of the current "slice"
+        for h in range(n_H):    # 类似于循环高
             vert_start = stride * h
             vert_end = vert_start + f
 
-            for w in range(n_W):  # loop over horizontal axis of the output volume
-                # Find the horizontal start and end of the current "slice"
+            for w in range(n_W):  # 类似于循环宽
                 horiz_start = stride * w
                 horiz_end = horiz_start + f
 
-                for c in range(n_C):  # loop over channels (= #filters) of the output volume
+                for c in range(n_C):  # 循环输出的通道数
 
-                    # Use the corners to define the (3D) slice of a_prev_pad (See Hint above the cell).
+                    # 获取单步矩阵
                     a_slice_prev = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end, :]
 
-                    # Convolve the (3D) slice with the correct filter W and bias b, to get back one output neuron.
                     weights = W[:,:,:,c]
                     biases = b[:,:,:,c]
                     Z[i, h, w, c] = conv_single_step(a_slice_prev, weights, biases)
 
-    # Making sure your output shape is correct
     assert (Z.shape == (m, n_H, n_W, n_C))
 
-    # Save information in "cache" for the backprop
     cache = (A_prev, W, b, hparameters)
 
     return Z, cache
@@ -149,61 +125,47 @@ def conv_forward(A_prev, W, b, hparameters):
 
 def pool_forward(A_prev, hparameters, mode="max"):
     """
-    Implements the forward pass of the pooling layer
-
-    Arguments:
-    A_prev: Input data, numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
-    hparameters: python dictionary containing "f" and "stride"
-    mode: the pooling mode you would like to use, defined as a string ("max" or "average")
-
-    Returns:
-    A: output of the pool layer, a numpy array of shape (m, n_H, n_W, n_C)
-    cache: cache used in the backward pass of the pooling layer, contains the input and hparameters
+    与卷积层类似，只是池化层选取后计算的矩阵的平均值或者最大值
+    :param A_prev: 完全的原矩阵
+    :param hparameters: 其他参数，如步长、填充空白
+    :param mode: 选用池化的类型average/max
+    :return:
     """
-
-    # Get dimensions from the input shape
+    # 原矩阵维度
     (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
 
-    # Get hyperparameters from "hparameters"
     f = hparameters["f"]
     stride = hparameters["stride"]
 
-    # Define the dimensions of the output
     n_H = int(1 + (n_H_prev - f) / stride)
     n_W = int(1 + (n_W_prev - f) / stride)
     n_C = n_C_prev
 
-    # Initialize output matrix A
+    # 预设应返回的经过卷积核后的矩阵大小
     A = np.zeros((m, n_H, n_W, n_C))
 
-    for i in range(m):  # loop over the training examples
-        for h in range(n_H):  # loop on the vertical axis of the output volume
-            # Find the vertical start and end of the current "slice" (≈2 lines)
+    for i in range(m):  # 循环所有
+        for h in range(n_H):  # 类似于循环高
             vert_start = h * stride
             vert_end = vert_start + f
 
-            for w in range(n_W):  # loop on the horizontal axis of the output volume
-                # Find the vertical start and end of the current "slice" (≈2 lines)
+            for w in range(n_W):  # 类似于循环宽
                 horiz_start = w * stride
                 horiz_end = horiz_start + f
 
-                for c in range(n_C):  # loop over the channels of the output volume
-
-                    # Use the corners to define the current slice on the ith training example of A_prev, channel c. (≈1 line)
+                for c in range(n_C):  # 循环输出的通道数
+                    # 获取单步矩阵
                     a_prev_slice = A_prev[i, vert_start:vert_end, horiz_start:horiz_end,c]
 
-                    # Compute the pooling operation on the slice.
-                    # Use an if statement to differentiate the modes.
-                    # Use np.max and np.mean.
+                    # 为单步矩阵做对应选择运算
                     if mode == "max":
                         A[i, h, w, c] = np.max(a_prev_slice)
                     elif mode == "average":
                         A[i, h, w, c] = np.mean(a_prev_slice)
 
-    # Store the input and hparameters in "cache" for pool_backward()
+    # 为池化层的反向传输储存元数据
     cache = (A_prev, hparameters)
 
-    # Making sure your output shape is correct
     assert (A.shape == (m, n_H, n_W, n_C))
 
     return A, cache
