6e5b6a5f2f2690c44b5efa7f4a787e885f989d13
bfmq <617351843@qq.com>
2019-11-26 22:47:30 +0800

    无标题时判断处理

--- day5/automatic_summary/core/get_sentences.py
+++ day5/automatic_summary/core/get_sentences.py
@@ -47,7 +47,7 @@ class SifEmbedding(object):
             self.sub_sentences, self.sub_punctuates = self.split_sentences(self.text)
             self.sub_sentences_vec = self.sif_sentence_embedding()
             self.sentences_vec = self.get_weight_average(self.text)
-            self.title_vec = self.get_weight_average(self.title) if self.title else []
+            self.title_vec = self.get_weight_average(self.title) if self.title else [None]
             self.sentences_corr_list = self.get_sentences_corr()
 
     def get_summarization(self):
@@ -194,7 +194,7 @@ class SifEmbedding(object):
             # 分别获取于正文本、标题之间的得分，按权重得到最后分数
             sub_sentence_vec = self.sub_sentences_vec[i, :]
             cosine_score_by_text = cosine(self.sentences_vec, sub_sentence_vec)
-            cosine_score_by_title = cosine(self.title_vec, sub_sentence_vec) if self.title_vec else 0
+            cosine_score_by_title = cosine(self.title_vec, sub_sentence_vec) if all(self.title_vec) else 0
             cosine_score = 0.9 * cosine_score_by_text + 0.1 * cosine_score_by_title
             if not isnan(cosine_score):  # 这里必须用isnan才能判断nan
                 corr_score_list.append([cosine_score, self.sub_sentences[i]])
@@ -225,7 +225,7 @@ class SifEmbedding(object):
         :return: 关键字列表
         """
         text_vec = self.remove_pca(self.title_vec.reshape(1, -1)) \
-            if self.title_vec else self.remove_pca(self.sentences_vec.reshape(1, -1))
+            if all(self.title_vec) else self.remove_pca(self.sentences_vec.reshape(1, -1))
 
         words = cut(self.title).split() if self.title else self.text
         words = [w for w in words if w in model]
